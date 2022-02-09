ae25533b22922cc009f3bb63808ab62d8758dbf8
Matt Jones <al2o3cr@gmail.com>
2022-02-09 12:40:38 -0500

    activerecord exercises

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,21 @@
 class Movie < ApplicationRecord
   validates_presence_of :title
+
+  def self.titles
+    # all.map { |movie| movie.title }
+    # or
+    pluck(:title)
+  end
+
+  def self.sorted_by_facebook_likes
+    order(facebook_likes: :desc)
+  end
+
+  def self.release_year_by_title(target_title)
+    find_by(title: target_title)&.year
+  end
+
+  def self.count_with_facebook_likes(target_likes)
+    where("facebook_likes > ?", target_likes).count
+  end
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -11,4 +11,45 @@ class MovieTest < ActiveSupport::TestCase
     movie = Movie.new(title: " ")
     refute movie.valid?
   end
+
+  # Make a method that returns an array of just movie titles.
+  #
+  test "lists movie titles" do
+    Movie.create!(title: "Movie 1")
+    Movie.create!(title: "Movie 2")
+
+    titles = Movie.titles
+
+    assert_equal ["Movie 1", "Movie 2"], titles
+  end
+
+  # Make a method that returns all movies in order of most to least Facebook likes.
+  test "lists movies in order of facebook likes" do
+    movie1 = Movie.create!(title: "Movie 1", facebook_likes: 100)
+    movie2 = Movie.create!(title: "Movie 2", facebook_likes: 2)
+
+    movies = Movie.sorted_by_facebook_likes
+
+    assert_equal [movie1, movie2], movies
+  end
+
+  # Make a method that can find a movie by its title, and tell you what year it was released.
+  test "find a movies by title and return the year" do
+    Movie.create!(title: "Movie 1", year: "2022")
+    Movie.create!(title: "Movie 2", year: "1982")
+
+    year = Movie.release_year_by_title("Movie 1")
+
+    assert_equal "2022", year
+  end
+
+  # Make a method that returns the number of movies in the database with more than a given number of Facebook likes
+  test "counts movies with more facebook likes" do
+    Movie.create!(title: "Movie 1", facebook_likes: 100)
+    Movie.create!(title: "Movie 2", facebook_likes: 2)
+
+    count = Movie.count_with_facebook_likes(10)
+
+    assert_equal 1, count
+  end
 end
