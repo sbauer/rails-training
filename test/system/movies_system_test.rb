bd5faf26c39b3b19d7308b39b9c3f0375c356aaa
Matt Jones <al2o3cr@gmail.com>
2022-02-09 13:42:22 -0500

    wire up Movie model to controller

--- test/system/movies_system_test.rb
+++ test/system/movies_system_test.rb
@@ -6,8 +6,9 @@ class MoviesSystemTest < ApplicationSystemTestCase
     # when I visit /movies/1
     # I see the title of the movie "Parasite"
     # I also see the name of the director "Bong Joon-ho"
+    movie = Movie.create!(title: "Parasite", director: "Bong Joon-hoo")
 
-    visit movie_path(1)
+    visit movie_path(movie.id)
     assert_text "Parasite"
     assert_text "Bong Joon-ho"
   end
@@ -16,8 +17,9 @@ class MoviesSystemTest < ApplicationSystemTestCase
     # As a user,
     # When I visit a movie with the id of 2,
     # I see the title for that movie
+    movie = Movie.create!(title: "Other Movie")
 
-    visit movie_path(2)
+    visit movie_path(movie.id)
     assert_text "Other Movie"
   end
 end
