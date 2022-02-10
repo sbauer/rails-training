48fb6961120f9aad25fc529cedc24ab5053cd246
Matt Jones <al2o3cr@gmail.com>
2022-02-10 16:22:36 -0500

    add Director model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_10_190130) do
+ActiveRecord::Schema.define(version: 2022_02_10_211533) do
+
+  create_table "directors", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
