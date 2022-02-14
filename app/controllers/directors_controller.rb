9fe9eab3aa08201b1a63dd6448078cdf0562060b
Matt Jones <al2o3cr@gmail.com>
2022-02-14 15:11:26 -0500

    switch to local variable style

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,71 +1,67 @@
 class DirectorsController < ApplicationController
-  before_action :set_director, only: %i[ show edit update destroy ]
-
   # GET /directors or /directors.json
   def index
-    @directors = Director.all
+    directors = Director.all
+
+    render locals: { directors: directors }
   end
 
   # GET /directors/1 or /directors/1.json
   def show
-    @movies = @director.movies.to_a
+    director = Director.find(params[:id])
+    movies = director.movies.to_a
+
+    render locals: { director: director, movies: movies }
   end
 
   # GET /directors/new
   def new
-    @director = Director.new
+    director = Director.new
+
+    render locals: { director: director }
   end
 
   # GET /directors/1/edit
   def edit
+    director = Director.find(params[:id])
+
+    render locals: { director: director }
   end
 
   # POST /directors or /directors.json
   def create
-    @director = Director.new(director_params)
+    director = Director.new(director_params)
 
-    respond_to do |format|
-      if @director.save
-        format.html { redirect_to director_url(@director), notice: "Director was successfully created." }
-        format.json { render :show, status: :created, location: @director }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @director.errors, status: :unprocessable_entity }
-      end
+    if director.save
+      redirect_to director_url(director), notice: "Director was successfully created."
+    else
+      render :new, locals: { director: director }, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /directors/1 or /directors/1.json
   def update
-    respond_to do |format|
-      if @director.update(director_params)
-        format.html { redirect_to director_url(@director), notice: "Director was successfully updated." }
-        format.json { render :show, status: :ok, location: @director }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @director.errors, status: :unprocessable_entity }
-      end
+    director = Director.find(params[:id])
+
+    if director.update(director_params)
+      redirect_to director_url(director), notice: "Director was successfully updated."
+    else
+      render :edit, locals: { director: director }, status: :unprocessable_entity
     end
   end
 
   # DELETE /directors/1 or /directors/1.json
   def destroy
-    @director.destroy
+    director = Director.find(params[:id])
 
-    respond_to do |format|
-      format.html { redirect_to directors_url, notice: "Director was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    director.destroy
+
+    redirect_to directors_url, notice: "Director was successfully destroyed."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_director
-      @director = Director.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def director_params
-      params.require(:director).permit(:name, :age)
-    end
+  def director_params
+    params.require(:director).permit(:name, :age)
+  end
 end
