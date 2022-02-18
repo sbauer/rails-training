c90fe0c76bf23489930aa36e7cfe5b5114bdfee1
Matt Jones <al2o3cr@gmail.com>
2022-02-18 14:54:22 -0500

    bump to 7.0 and run app:update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_11_165824) do
+ActiveRecord::Schema[6.1].define(version: 2022_02_11_165824) do
 
   create_table "directors", force: :cascade do |t|
     t.string "name"
